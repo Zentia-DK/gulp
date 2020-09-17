6a1f1f82b9f4c5e53e702f24b0186207ffbab138
LevisMonefeldt <zentia88@live.dk>
2020-09-17 11:00:22 +0200

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require("gulp");
+const sass= require("gulp-sass");
+
+
+function styles() {
+    return(
+        gulp.src("css/*.scss")
+            .pipe(sass())
+            .pipe(gulp.dest("css"))
+    );
+}
